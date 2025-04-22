83547b144c14e7442e4f6142f595974b91efee8d
mateus-po <mateusz.porebski99@gmail.com>
2025-04-22 22:01:48 +0200

    fix bug in checking orders expected state

--- app/services/orders/shared.py
+++ app/services/orders/shared.py
@@ -14,7 +14,7 @@ from app.models.special_offer import SpecialOffer
 from app.models.user import User
 
 
-def calculate_order_prices(order:Order, user:User, db_ref: firestore.Client) -> float:
+def calculate_order_prices(order: Order, user: User, db_ref: firestore.Client) -> float:
     order_items = order.order_items
     dish_ids = list(order_items.keys())
     dish_refs = [db_ref.collection(CollectionNames.DISHES).document(dish_id) for dish_id in dish_ids]
@@ -31,7 +31,8 @@ def calculate_order_prices(order:Order, user:User, db_ref: firestore.Client) -> 
     restaurant_doc = order.restaurant_id.get().to_dict()
 
     user_special_offers = [SpecialOffer(**doc.to_dict()) for doc in db_ref.get_all(user.special_offers)]
-    restaurant_special_offers = [SpecialOffer(**doc.to_dict()) for doc in db_ref.get_all(restaurant_doc.get('special_offers'))]
+    restaurant_special_offers = [SpecialOffer(**doc.to_dict()) for doc in
+                                 db_ref.get_all(restaurant_doc.get('special_offers'))]
 
     for special_offer in user_special_offers + restaurant_special_offers:
         dish_id = special_offer.dish_id.id
@@ -43,7 +44,6 @@ def calculate_order_prices(order:Order, user:User, db_ref: firestore.Client) -> 
     sum_order_total_special_offers = lambda acc, id: acc + dish_prices_including_special_offers[id] * order_items[id]
     total_including_discounts = reduce(sum_order_total_special_offers, dish_ids, 0.0)
     return order_total, total_including_discounts
-
 
 
 def check_restaurant_dishes_existence(order: CreateOrderPayload, db_ref: firestore.Client) -> None:
@@ -87,19 +87,22 @@ def check_order_validity_and_ownership(order_id: str,
             detail=f"No such order with id: {order_id}",
         )
 
-    if expected_state is not None and order_dict.get('state') == expected_state:
+    if expected_state is not None and order_dict.get('status') != expected_state.value:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f"Cannot process order with id: {order_id} - incorrect state: {order_dict.get('state')}, expected state: {expected_state}",
+            detail=f"Cannot process order with id: {order_id} - incorrect state: {order_dict.get('status')}, expected state: {expected_state.value}",
         )
 
     return PersistedOrder(**order_dict)
+
 
 def get_order_by_id(order_id: str, db_ref) -> PersistedOrder:
     result = PersistedOrder(**db_ref.collection(CollectionNames.ORDERS).document(order_id).get().to_dict())
     return result
 
+
 from google.cloud.firestore import Transaction
+
 
 def finalize_order_stock(order: PersistedOrder, order_id: str, db_ref) -> bool:
     @firestore.transactional
