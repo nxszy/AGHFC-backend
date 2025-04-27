b7ed740c0df42f2a089ab633d30a69b71d996c0c
mateus-po <mateusz.porebski99@gmail.com>
2025-04-27 20:06:56 +0200

    connect dishes and restaurant dishes routers to the main router

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from fastapi import Depends, FastAPI
 from fastapi.security import HTTPBearer
 
 from app.core.middleware import AuthMiddleware
+from app.routers.dishes import mobile as dishes_mobile
+from app.routers.dishes import panel as dishes_panel
 from app.routers.orders import mobile as orders_mobile
 from app.routers.orders import panel as orders_panel
 from app.routers.orders import worker_panel as orders_worker_panel
+from app.routers.restaurant_dishes import panel as restaurant_dishes_panel
 from app.routers.restaurants import mobile as restaurant_mobile
 from app.routers.restaurants import panel as panel_mobile
 from app.routers.special_offers import mobile as special_offers_mobile
@@ -30,3 +33,6 @@ app.include_router(special_offers_mobile.router)
 app.include_router(special_offers_panel.router)
 app.include_router(workers_panel.router)
 app.include_router(worker_panel.router)
+app.include_router(dishes_mobile.router)
+app.include_router(dishes_panel.router)
+app.include_router(restaurant_dishes_panel.router)
