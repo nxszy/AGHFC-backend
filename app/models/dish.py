09fce68fbbd694d56f56516bbce7b6170fb3cca1
nxszy <nizinski.szy@gmail.com>
2025-05-22 16:09:26 +0200

    Added dish_id to response

--- app/models/dish.py
+++ app/models/dish.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel
+from typing import Optional
+from app.models.firestore_ref import FirestoreRef
+from pydantic import BaseModel, Field
 
 
 class Dish(BaseModel):
+    id: Optional[str] = Field(default=None, description="Firestore document ID")
     name: str
     description: str
     ingredients: str
     price: float
     points: int
+
+    class Config:
+        json_encoders = {
+            FirestoreRef: lambda v: v.ref.id
+        }
